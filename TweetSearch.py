207d70dd504a8e8d231b556c9634763d3479dbe1
admin <admin@PharaohsCat-MacAir.local>
2018-03-21 18:42:40 +0900

    First commit on TweetSearch

---
+++ TweetSearch.py
@@ -0,0 +1,21 @@
+# Twitter search to Excel
+#   '''Search for thousands of tweets and accounts
+#Perform bulk searches on multiple terms
+# Retrieve 50+ columns per tweet & account
+#mRemove duplicate tweets automatically'''
+
+# You just need to call Twitter api which you need to read about
+# from twitter developers website and then use a xlwings library that will do
+# the work you want because it is developed to scrape the information from
+# website and then enter that information into the excel sheet in a proper format.
+
+# Install the twitter package: Python Package Index
+# Install openpyxl: openpyxl - A Python library to read/write Excel 2010 xlsx/xlsm files - openpyxl 2.4.8 documentation
+# Use the twitter module to gather the data
+# Use the openpyxl package to write it to Excel
+
+# Check out ideoforms/python-twitter-examples
+# for practical examples of Twitter API.
+
+# PACKAGES: platformio-ide-terminal; script
+# stage
